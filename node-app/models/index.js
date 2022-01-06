1265ed1e6357ce7fb9ac1a0b3913762afe99d7fc
alekhinefan <szavinm@yahoo.com>
2022-01-06 17:34:58 +0100

    register user model

--- node-app/models/index.js
+++ node-app/models/index.js
@@ -6,5 +6,6 @@ const db = {}
 db.mongoose = mongoose
 db.url = dbConfig.url
 db.artists = require('./artists.model.js')(mongoose)
+db.users = require('./users.model.js')(mongoose)
 
 module.exports = db

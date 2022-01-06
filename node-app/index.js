36fd994a35b9191dd231183eea0e338a8d379b1f
alekhinefan <szavinm@yahoo.com>
2022-01-06 20:13:22 +0100

    additional routes

--- node-app/index.js
+++ node-app/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const cookiParser = require('cookie-parser')
 const connectToDb = require('./helpers/connectToDb')
-const { create, findAll, findByIdOrName } = require('./controllers/artists.controller.js')
+const { createArtist, findAll, findByIdOrName } = require('./controllers/artists.controller.js')
+const { createUser, login } = require('./controllers/users.controller.js')
 
 const app = express()
 let corsOptions = {
@@ -11,6 +13,7 @@ let corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cookiParser())
 
 const port = process.env.PORT || 8080
 
@@ -36,6 +39,36 @@ app.get('/artist/:id', async (req, res) => {
 
 app.post('/artists', async (req, res) => {
   console.log(req.body)
-  await create(req)
+  await createArtist(req)
   res.sendStatus(200)
+})
+
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body
+  const user = await createUser(email, password)
+  const _id = user?._id
+  if (!_id) {
+    res.sendStatus(409)
+  } else {
+    res.send({_id})
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  const loginResult = await login(email, password)
+
+  if (!loginResult.loginSuccessful) {
+    console.log(loginResult)
+    res.sendStatus(403)
+  } else {
+    console.log('successful login:', email)
+    res.cookie('user_id', loginResult._id, { httpOnly: true, maxAge: 3600000 })
+    res.sendStatus(200)
+  }
+})
+
+app.post('/signout', (req, res) => {
+  res.clearCookie("user_id")
+  res.redirect('/')
 })
